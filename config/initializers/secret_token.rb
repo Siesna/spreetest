69229bda690d8b68052e513c0fde3c209dce3ac4
Jaime <jaime@synaptian.com>
2013-07-25 15:37:03 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spreetest::Application.config.secret_token = '3cadd71c79e7e8a7276252d3cad32c630691d2b715ed997661f18750b9fcf17616c17f270e76c7f8e3bc8d84f32bf003ef001f5f5e68f5820dcfd42760e71979'
